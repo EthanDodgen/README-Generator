3d4609dae0a048c87fd66fac152f5c5b8ba6b36b
Fival <thefivalco@gmail.com>
2021-01-19 13:09:24 -0600

    initial commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,14 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+//const fs = require("fs")
+//const generatePage = require("./utils/generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+    type:
+    }
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +18,5 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+console.log(inquirer)
